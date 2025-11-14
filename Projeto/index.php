bd9e2970ba354b01151049ec18722a2d946bc50d
Gabriel Kloster <gklostermorais@gmail.com>
2025-11-14 17:07:10 -0300

    cadrastro.php TESTE COM BANCO DE DADOS e php

--- Projeto/index.php
+++ Projeto/index.php
@@ -8,26 +8,8 @@
 </head>
 <body>
      <div class="container flex centro-coluna centro-linha">
-        <div class="quadrado-secundario flex centro-linha arruma text-arr">
-            <div class="div-1"><H1>Seja bem vindo</H1>
-            </div>
-            <div class="div-2 flex arruma comeco-coluna">
-                <div class="flex centro-linha">
-                    <div class="flex centro-linha">
-                        <div><h2 class="padding">Acesse sua conta agora</h2></div>
-                    </div>
-                </div>
-
-
-            </div>
-
-            <div class="div-23 flex centro-linha">
-                <button class="botao-estilizar margin">entrar</button>
-            </div>
-
-            <div class="div-3">
-            </div>
-        </div>
+     <form action="process.php" method="POST" onsubmit="return validarform()">
+        
         <div class="quadrado-principal  ">
             <div class="div-34">
                 <h1 class="flex alinha-text estiliza-fonte"></h1>Bem vindo de volta</h1>
@@ -38,7 +20,7 @@
                     <div>
                         <img src="https://www.veryicon.com/icons/miscellaneous/rossi-icon-library/user-name-19.html" alt="Carregando">
                     </div>
-                     <input class="input-style" type="text" placeholder="NOME COMPLETO" id="nome_completo">
+                     <input class="input-style" type="text" placeholder="email" name="email">
                 </div>
             </div>
 
@@ -46,21 +28,42 @@
                 <div>
                     <img src="https://pngtree.com/so/email" alt="Carregando">
                 </div>
-                <input class="input-style" type="password" placeholder="SENHA" id="senha">
+                <input class="input-style" type="password" placeholder="SENHA" name="senha">
             </div>
 
             <div class="div-6 flex comeco-coluna">
                  <div>
                     <img src="https://pngtree.com/so/icon-password" alt="carregando">
                  </div>
-                 <input class="input-style" type="password" placeholder="CONFIRMAR SENHA" id="confirmar_senha">
+                 <input class="input-style" type="password" placeholder="CONFIRMAR SENHA" name="confirmaSenha">
             </div>
 
             <div class="div-7 flex comeco-coluna">
-                <button class="botao-estilizar-2" onclick="cadrastrar()">LOGIN</button>
+                <input type="submit" value="Enviar">
             </div>
         </div>
+     </form>
      </div>
     <script src="script.js"></script>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de cadrastro</title>
+</head>
+<body>
+    <h1>cadrastro de usuario</h1>
+    
+    <form action="process.php" method="POST" onsubmit="return validarform()">
+        <input type="email" placeholder="email. . ." name="email">
+        <input type="password" placeholder="senha. . ." name="senha">
+        <input type="password" placeholder="confirmar senha. . ." name="confirmaSenha">
+        <input type="submit" value="Enviar">
+    </form>
+    
 </body>
 </html>
