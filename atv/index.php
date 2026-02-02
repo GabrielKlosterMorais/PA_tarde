7e53339863a155f0e5f093d9d6067db3009da841
gabriel kloster <gklostermorais@gmail.com>
2026-02-02 17:17:32 -0300

    Atv de php e css

---
+++ atv/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atv</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container space">
+        <div class="metade esquerda">
+            <img src="https://preview.redd.it/ugywjdjkaq961.png?width=640&crop=smart&auto=webp&s=07babfb3a6035911fbe7ff0446dec816c25cd8db" alt="gato gritando em puro sofrimento" width= "100%" height="100%" >
+        </div>
+
+         <div class="metade azul flex coluna">
+            <div class="umQuinto meioLinha meioColuna flex">
+                <input type="text" placeholder="nome" class="input">
+            </div>
+            
+            <div class="umQuinto meioLinha meioColuna flex">
+                <input type="text" placeholder="cpf" class="input">
+            </div>
+            
+            <div class="umQuinto meioLinha meioColuna flex">
+                <input type="date" placeholder="data de nascimento" class="input">
+            </div>
+            
+            <div class="umQuinto meioLinha meioColuna flex">
+                 <input type="password" placeholder="senha" class="input">
+            </div>
+           
+            <div class="umQuinto meioLinha meioColuna flex">
+                <input type="submit" palaceholder="enviar" class="input1">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
